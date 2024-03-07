ee5d3cb2f36d2c60d18d7635e69f319118f660f0
Slee Woo <mail@sleewoo.com>
2024-03-07 18:14:52 +0200

    refined worker pool

--- lib/worker-pool.ts
+++ lib/worker-pool.ts
@@ -1,40 +1,42 @@
 import { join } from "path";
 import { Worker, isMainThread, parentPort } from "worker_threads";
 
-import pkg from "../package.json";
-import * as fetchGenerator from "./fetch-generator/worker";
+import * as apiGenerator from "./api-generator/workers";
+import * as apiAssets from "./api-assets/workers";
+import * as fetchGenerator from "./fetch-generator/workers";
+import * as viewsGenerator from "./views-generator/workers";
 
-let worker: InstanceType<typeof Worker> | undefined;
+export let worker: InstanceType<typeof Worker> | undefined;
+
+export const workerPool = {
+  apiGenerator,
+  apiAssets,
+  fetchGenerator,
+  viewsGenerator,
+} as const;
 
 if (isMainThread) {
   worker = new Worker(join(__dirname, "worker.js"));
+
   worker.on("exit", (code) => {
-    console.error(`\n[ ${pkg.name} ]: Worker Exited with code ${code}`);
+    console.error(`\nWorker Exited with code ${code}`);
     worker = undefined;
   });
-}
 
-const workerMap = {} as { fetchGenerator: typeof fetchGenerator };
-
-for (const [pool, workers] of Object.entries({ fetchGenerator })) {
-  // @ts-ignore
-  workerMap[pool] = Object.keys(workers).reduce(
-    (map: Record<string, (data: unknown) => void>, task) => {
-      if (isMainThread) {
+  for (const [pool, workers] of Object.entries(workerPool)) {
+    // @ts-expect-error
+    workerPool[pool] = Object.keys(workers).reduce(
+      (map: Record<string, (data: unknown) => void>, task) => {
         map[task] = (data) => worker?.postMessage({ pool, task, data });
-      } else {
-        parentPort?.on("message", async (msg) => {
-          if (msg.pool !== pool || msg.task !== task) {
-            return;
-          }
-          // @ts-ignore
-          await workers[task](msg.data);
-        });
-      }
-      return map;
-    },
-    {},
-  );
+        return map;
+      },
+      {},
+    );
+  }
+} else {
+  parentPort?.on("message", async (msg) => {
+    const { pool, task, data } = msg;
+    // @ts-expect-error
+    await workerPool[pool]?.[task]?.(data);
+  });
 }
-
-export default workerMap;
