517c364865c1b090d43e276c8ad2083965752d80
Slee Woo <mail@sleewoo.com>
2024-03-07 18:14:42 +0200

    chore

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,9 @@
 export { defaults } from "./defaults";
 export * from "./api-generator";
 export * from "./api-handler";
+export * from "./api-assets";
 export * from "./fetch-generator";
 export * from "./define";
-export * from "./views";
+export * from "./views-generator";
 export * from "./vue-plugin-generator";
 export * from "./file-bundler";
